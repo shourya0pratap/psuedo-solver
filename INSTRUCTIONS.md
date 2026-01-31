9f6dde05842de17443f8c0cf01b3c07860bdc552
sp_3141 <07shourya.pratap@gmail.com>
2026-01-31 23:13:42 +0530

    Added instructions.md and refactored some code

---
+++ INSTRUCTIONS.md
@@ -0,0 +1,29 @@
+# Instructions for Psuedo Code
+
+# Assignment Operations
+
+- LET [`dataType`] [`variableName`] BE [`val`]
+- SET [`variableName`] TO [`val`]
+
+# Arithmetic Operations
+
+- ADD [`val\var`] TO [`target`]
+- SUB [`val\var`] FROM [`target`]
+- MUL [`target`] WITH [`val\var`]
+- DIV [`target`] BY [`val\var`]
+- EXP [`target`] TO [`y`]
+
+# Relational
+TBA
+
+# Comparative
+TBA
+
+# Conditional
+TBA
+
+# Iterative
+TBA
+
+# Input \ Output
+TBA
--- script.py
+++ script.py
@@ -1,6 +1,20 @@
 # dictionary to store variables as key value pairs
 variables = {}
 
+def getValue(token:str):
+    if token in variables:
+        return variables[token]
+    if token.startswith('"') and token.endswith('"'):
+        return token.strip('"')
+    if token == "True": return True
+    if token == "False": return False
+    try:
+        if "." in token:
+            return float(token)
+        return int(token)
+    except ValueError:
+        print(f"ERROR: '{token}' is not a defined variable or valid number.")
+
 def createVariable(instruction):
     type = instruction[1]
     var = instruction[2]
@@ -16,35 +30,40 @@ def createVariable(instruction):
             val = bool(val)
     variables[var] = val
 
+def setVariable(instruction):
+    var = instruction[1]
+    val = getValue(instruction[3])
+    variables[var] = val
+
 def add(instruction):
-    var1 = instruction[1]
-    var2 = instruction[3]
-    # add var1 TO var2
-    variables[var2] += variables[var1]
+    val = getValue(instruction[1])
+    var = instruction[3]
+    # add val TO var
+    variables[var] += val
     
 def sub(instruction):
-    var1 = instruction[1]
-    var2 = instruction[3]
-    # subtract var1 FROM var2
-    variables[var2] -= variables[var1]
+    val = getValue(instruction[1])
+    var = instruction[3]
+    # subtract val FROM var
+    variables[var] -= val
     
 def mul(instruction):
-    var1 = instruction[1]
-    var2 = instruction[3]
-    # multiply var1 WITH var2
-    variables[var1] *= variables[var2]
+    val = getValue(instruction[1])
+    var = instruction[3]
+    # multiply var WITH val
+    variables[var] *= val
     
 def div(instruction):
-    var1 = instruction[1]
-    var2 = instruction[3]
-    # divide var1 BY var2
-    variables[var1]/=variables[var2]
+    val = getValue(instruction[1])
+    var = instruction[3]
+    # divide var BY val
+    variables[var] /= val
 
 def exp(instruction):
-    var1 = instruction[1]
-    var2 = instruction[3]
-    # raise var1 TO var2
-    variables[var1] **= variables[var2]
+    val = getValue(instruction[1])
+    var = instruction[3]
+    # raise var TO val
+    variables[var] **= val
 
 def main(st:str):
     instructions =  st.split("\n")  
@@ -53,6 +72,8 @@ def main(st:str):
         match(instructStr[0]):
             case "LET":
                 createVariable(instructStr)
+            case "SET":
+                setVariable(instructStr)
             case "ADD":
                 add(instructStr)
             case "SUB":
@@ -64,5 +85,5 @@ def main(st:str):
             case "EXP":
                 exp(instructStr)
         print(instruction, variables)  
-st = "LET INT x BE 5\nLET FLOAT y BE 10\nADD x TO y"
+st = "LET INT x BE 5\nLET FLOAT y BE 10\nSET x TO y"
 main(st)
